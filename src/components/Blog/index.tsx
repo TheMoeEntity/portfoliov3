988f0b2525f0fa45cdb28eae6e9e37a6b6f8ec75
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-03-07 23:28:51 +0100

    Add author section to blog post page

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import styles from "../../app/page.module.css";
 import { blogPostType } from "@/Helpers/types";
+import me from '../../../public/images/moe_profile.jpg'
 
 const Blog = ({ single }: { single: blogPostType }) => {
     return (
@@ -65,6 +66,34 @@ const Blog = ({ single }: { single: blogPostType }) => {
                                 <strong>{single.end}</strong>
                             </div>
 
+                            <div className={styles.authorSect}>
+                                <div>
+                                    <Image
+                                        src={me}
+                                        style={{
+                                            objectFit: "cover",
+                                        }}
+                                        alt={
+                                            "Picture of me as author of blog post"
+                                        }
+                                        fill
+                                        quality={100}
+                                        priority={true}
+                                        sizes="(max-width: auto) 100vw, (max-width: auto) 50vw, 33vw"
+                                    />
+                                </div>
+                                <div>
+                                    <aside>
+                                        <strong>Written by: Moses Nwigberi.</strong>
+                                    </aside>
+                                    <p>
+                                        Hello, I'm Moe, and I hope you enjoyed reading this post as much as I enjoyed writing it! If you have any questions or feedback, feel free to reach out. Thanks for taking the time to explore the world of frontend development with me. Stay curious and keep coding! ;) 🚀
+                                    </p>
+                                    <aside>
+                                        {single.date}
+                                    </aside>
+                                </div>
+                            </div>
 
                         </div>
                     </div>
